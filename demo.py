ae92741d72ca6bc4bd7e3118279aa7bb5c5a78fb
mitalijaju25 <mitalijaju75@gmail.com>
2025-06-25 13:36:00 +0530

    Corrected message

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
 print("Hello World")
 
 print ("This is updated using VS Code")
+
+print ("GitHub and VSCode ecosystem has now been set up, and these changes will be reflected in the GitHub main branch of this repository")
+
+import math
+
+def area_of_circle(radius):
+    return math.pi * radius ** 2
